8ea0a23e48cfa5930fa92c20189541d8fda9ce50
JoseLRiano <joselriano@yahoo.com>
2020-05-15 02:26:00 -0400

    Updates

---
+++ precache-manifest.c49701207f9ea64d94a79e5be747e59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ca7e0d815e2fafb6599041ba0c64b1",
+    "url": "/ReactStateLottery/index.html"
+  },
+  {
+    "revision": "24aaca819a53f019eef2",
+    "url": "/ReactStateLottery/static/css/main.459e1771.chunk.css"
+  },
+  {
+    "revision": "ae6e1a532ea319b2e5dd",
+    "url": "/ReactStateLottery/static/js/2.a43e5543.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactStateLottery/static/js/2.a43e5543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24aaca819a53f019eef2",
+    "url": "/ReactStateLottery/static/js/main.f3e69913.chunk.js"
+  },
+  {
+    "revision": "cac48968489307f2ee49",
+    "url": "/ReactStateLottery/static/js/runtime-main.252175a6.js"
+  }
+]);
